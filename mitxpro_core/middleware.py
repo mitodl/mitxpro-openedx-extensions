67a8175b965bb937af69583b82f86b6e6e2b958d
Asad Ali <asad.ali@arbisoft.com>
2023-11-09 00:11:09 +0500

    fix: Upgrade Django to support v4 (#19)
    * Upgrade Django to support v4
    
    * update poetry
    
    * fix test impoort
    
    * fix pylint
    
    * remove idea
    
    * black format
    
    * formatting
    
    * isort

--- mitxpro_core/middleware.py
+++ mitxpro_core/middleware.py
@@ -4,13 +4,17 @@ import re
 from django.conf import settings
 from django.shortcuts import redirect
 from django.utils.deprecation import MiddlewareMixin
-from django.utils.http import urlquote
 
 
 try:
     from urllib.parse import urlsplit, urlunsplit, parse_qsl
 except ImportError:
     from urlparse import urlsplit, urlunsplit, parse_qsl
+
+try:
+    from django.utils.http import urlquote
+except ImportError:
+    from urllib.parse import quote as urlquote  # pylint: disable=ungrouped-imports
 
 
 def redirect_to_login(request):
@@ -20,7 +24,12 @@ def redirect_to_login(request):
     )
     query = parse_qsl(query)
     query.append(("next", urlquote(request.build_absolute_uri())))
-    query = "&".join(["{}={}".format(key, value) for (key, value) in query])
+    query = "&".join(
+        [
+            "{}={}".format(key, value)  # pylint: disable=consider-using-f-string
+            for (key, value) in query
+        ]
+    )
     return redirect(urlunsplit((scheme, netloc, path, query, fragment)))
 
 
@@ -34,14 +43,14 @@ class RedirectAnonymousUsersToLoginMiddleware(MiddlewareMixin):
         ):
             # if allowed regexes are set, redirect if the path doesn't match any
             allowed_regexes = settings.MITXPRO_CORE_REDIRECT_ALLOW_RE_LIST
-            if allowed_regexes and not any(
+            if allowed_regexes and not any(  # pylint: disable=use-a-generator
                 [re.match(pattern, request.path) for pattern in allowed_regexes]
             ):
                 return redirect_to_login(request)
 
             # if denied regexes are set, redirect if the path matches any
             denied_regexes = settings.MITXPRO_CORE_REDIRECT_DENY_RE_LIST
-            if denied_regexes and any(
+            if denied_regexes and any(  # pylint: disable=use-a-generator
                 [re.match(pattern, request.path) for pattern in denied_regexes]
             ):
                 return redirect_to_login(request)
--- mitxpro_core/middleware_test.py
+++ mitxpro_core/middleware_test.py
@@ -1,6 +1,11 @@
 """Middleware tests"""
 import pytest
-from django.utils.http import urlquote
+
+
+try:
+    from django.utils.http import urlquote
+except ImportError:
+    from urllib.parse import quote as urlquote
 
 
 @pytest.mark.parametrize("is_enabled", [True, False])
@@ -51,9 +56,12 @@ def test_redirect_middleware(
     if should_redirect:
         mock_get_response.assert_not_called()
         assert response.status_code == 302
-        assert response.url == "{}&next={}".format(
-            settings.MITXPRO_CORE_REDIRECT_LOGIN_URL,
-            urlquote(request.build_absolute_uri()),
+        assert (
+            response.url
+            == "{}&next={}".format(  # pylint: disable=consider-using-f-string
+                settings.MITXPRO_CORE_REDIRECT_LOGIN_URL,
+                urlquote(request.build_absolute_uri()),
+            )
         )
     else:
         mock_get_response.assert_called_once_with(request)
